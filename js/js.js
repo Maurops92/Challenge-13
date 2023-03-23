4665f2ce024af46a9fd0c4805cd14c0804d60515
Mauro Gaston Perez Gianolini <pmora92@hotmail.com>
2023-03-23 14:06:55 -0300

    Ejercicios Javascript

---
+++ js/js.js
@@ -0,0 +1,26 @@
+let numero1 = 4
+let numero2 = 5
+let resultado = undefined
+
+resultado = numero1 + numero2;
+document.write(resultado);
+document.write("<br>");
+
+resultado = numero1 - numero2;
+document.write(resultado);
+document.write("<br>");
+
+resultado = numero1 * numero2;
+document.write(resultado);
+document.write("<br>");
+
+resultado = numero1 / numero2;
+document.write(resultado);
+document.write("<br>");
+
+resultado = numero1 % 2;
+document.write("El valor del resto es " + resultado);
+document.write("<br>");
+
+resultado = numero2 % 2;
+document.write("El valor del resto es " + resultado);
