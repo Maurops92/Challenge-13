22ee76209dccb54dbe0a25c7241e2ac7c40d0136
Mauro Gaston Perez Gianolini <pmora92@hotmail.com>
2023-03-23 17:07:24 -0300

    Ejercicio JS terminado

--- js/js.js
+++ js/js.js
@@ -1,21 +1,21 @@
 let numero1 = 4
 let numero2 = 5
-let resultado = undefined
+let numero3 = 7
 
-resultado = numero1 + numero2;
-document.write(resultado);
+let suma = numero1 + numero2;
+document.write(suma);
 document.write("<br>");
 
-resultado = numero1 - numero2;
-document.write(resultado);
+let resta = numero1 - numero2;
+document.write(resta);
 document.write("<br>");
 
-resultado = numero1 * numero2;
-document.write(resultado);
+let multiplicacion = numero1 * numero2;
+document.write(multiplicacion);
 document.write("<br>");
 
-resultado = numero1 / numero2;
-document.write(resultado);
+let division = numero1 / numero2;
+document.write(division);
 document.write("<br>");
 
 resultado = numero1 % 2;
@@ -24,3 +24,18 @@ document.write("<br>");
 
 resultado = numero2 % 2;
 document.write("El valor del resto es " + resultado);
+document.write("<br>");
+
+document.write("Si el resto es 0 el numero es par y si es distinto de 0 es impar");
+
+let suma2= suma + numero3;
+document.write(suma2);
+document.write("<br>");
+
+let resta2= resta - numero3;
+document.write(resta2);
+document.write("<br>");
+
+let mult2= multiplicacion * numero3;
+document.write(mult2);
+document.write("<br>");
